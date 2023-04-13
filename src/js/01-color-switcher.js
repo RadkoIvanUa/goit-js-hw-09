49da783f550ad88a0db7ddf3cca6b8c3610fe2ab
Ivan Radko <mr.radkoivan@gmail.com>
2023-04-13 15:46:26 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.stopBtn.setAttribute('disabled', 'disabled');
+
+refs.startBtn.addEventListener('click', onStart);
+
+function onStart() {
+  const intervalId = setInterval(() => {
+    refs.stopBtn.addEventListener('click', onStop);
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled', 'disabled');
+  }, 1000);
+  function onStop() {
+    clearInterval(intervalId);
+    refs.startBtn.removeAttribute('disabled', 'disabled');
+    refs.stopBtn.setAttribute('disabled', 'disabled');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
